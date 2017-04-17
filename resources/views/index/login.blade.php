808f3638f744613fe85d585aa0f80693fdc488d0
warthur <warthur@163.com>
2017-04-17 17:18:46 +0800

    图片

--- resources/views/index/login.blade.php
+++ resources/views/index/login.blade.php
@@ -13,6 +13,13 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>
+        .login {
+            background-image: url({{asset('/resources/static/timg.jpg')}});
+            background-size: 100% 100%;
+            position: relative;
+        }
+    </style>
 </head>
 <body class="hold-transition login-page login">
 <div class="login-box">
