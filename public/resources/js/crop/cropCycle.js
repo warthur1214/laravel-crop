26d5f6f9ac23873eb90eff8d7f34f235d0f419b8
warthur <warthur@163.com>
2017-04-20 13:31:38 +0800

    页面

---
+++ public/resources/js/crop/cropCycle.js
@@ -0,0 +1,86 @@
+$(function () {
+    var form = $('#info_form');
+    form.find('.form-control').not('[nowrap="nowrap"]').wrap('<div class="form-group"></div>');
+    form.validate({
+        errorElement: 'label', //default input error message container
+        errorClass: 'text-red', // default input error message class
+        focusInvalid: false, // do not focus the last invalid input
+        rules: {
+            module_name: {
+                required: true
+            },
+            module_matched_key: {
+                required: true
+            },
+            module_order: {
+                maxlength: 3
+            }
+        },
+        invalidHandler: function (event, validator) { //display error alert on form submit
+            $('.alert-error').show();
+        },
+
+        highlight: function (element) { // hightlight error inputs
+            $(element).closest('.form-group').addClass('has-error'); // set error class to the control group
+        },
+
+        success: function (label) {
+            $('.alert-error').hide();
+            label.prev('.form-group').removeClass('has-error');
+            label.remove();
+        },
+
+        errorPlacement: function (error, element) {
+            error.addClass('text-red').insertAfter(element.closest('.form-group'));
+        },
+
+        submitHandler: function (form) {
+            $('.alert-error').hide();
+            $('.alert-success').show();
+        }
+    });
+
+    $('#submit').click(function () {
+        if (form.valid() == false) {
+            return false;
+        }
+        var formData = new FormData(form);
+        var cropImg = document.getElementById('cycle_img').files[0];
+        if (cropImg) {
+            formData.append("cycle_img", document.getElementById('cycle_img').files[0]);
+        }
+        formData.append("cycle_name", $("#cycle_name").val());
+        formData.append("cycle_describe", $("#cycle_describe").val());
+        formData.append("cycle_sort", $("#cycle_sort").val());
+        formData.append("cycle_section", $("#cycle_section").val());
+        var status = document.getElementsByName('cycle_status');
+        formData.append("cycle_status", status[1].checked ? 1 : 0);
+
+        $.ajax({
+            url: $("#app_url").val()+"/cycle/addCycleAjax",
+            type: "post",
+            data: formData,
+            dataType: "json",
+            processData: false,
+            contentType: false,
+            success: function (result) {
+                $('.alert').html(result.msg);
+                if (result.status == 0) {
+                    $('.alert').show();
+                }
+                else {
+                    $('.alert').show().removeClass('alert-error').addClass('alert-success');
+                    setTimeout(function () {
+                        window.location.href = $("#app_url").val()+'/cycle/cycleList'
+                    }, 2000);
+                }
+
+            }
+        });
+    });
+
+    $('#cycle_section').daterangepicker();
+    $('.fa-calendar-times-o').click(function () {
+        $('#cycle_section').val('');
+    });
+});
---
+++ resources/views/crop/cropCyle.blade.php
@@ -0,0 +1,152 @@
+@extends("base")
+
+@section("style")
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{asset("/resources/plugins/datatables/dataTables.bootstrap.css")}}">
+    <link rel="stylesheet" href="{{asset("/resources/plugins/daterangepicker/daterangepicker-bs3.css")}}">
+    <style type="text/css">
+        .img_browser {
+            height: 400px;
+            width: 300px;
+        }
+    </style>
+@endsection
+
+@section("body")
+    <body>
+    <section class="content-header">
+        <h1>
+            添加农产品周期属性
+        </h1>
+    </section>
+    <div class="box box-cus box-cus-form">
+        <div class="alert alert-error" style="display:none;">
+            <span>信息不能为空，请输入</span>
+        </div>
+        <div class="box-body">
+            <form role="form" id="info_form">
+                <table class="table table-bordered">
+                    <tbody>
+                    <tr class="form-group">
+                        <td class="title" colspan="2"><i class="fa fa fa-info-circle"></i> 添加周期属性</td>
+                        <input type="hidden" class="form-control" id="app_url" name="app_url"
+                               value="{{config('app.url')}}">
+                    </tr>
+                    <tr class="form-group">
+                        <th>选择周期 <span class="text-red">*</span></th>
+                        <td>
+                            <select name="cycle_id" id="cycle_id" class="form-group" title="选择生长周期">
+                                <option value="0">初始状态</option>
+                                @foreach($cycleInfo as $c)
+                                    <option value="{{$c['id']}}" {{$c['id']==$cropInfo['cycle_id'] ? 'selected' : ''}}>{{$c['cycle_name']}}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                    </tr>
+                    <tr class="form-group">
+                        <th>节点序号 <span class="text-red">*</span></th>
+                        <td>
+                            <input type="text" class="form-control" id="cycle_sort" name="cycle_sort"
+                                   placeholder="节点序号">
+                        </td>
+                    </tr>
+                    <tr class="form-group">
+                        <th>节点区间 <span class="text-red">*</span></th>
+                        <td>
+                            <div class="input-group-addon" style="float: left; width: 39px;height: 34px;">
+                                <i class="fa fa-calendar-times-o" style="margin-top: 4px;"></i>
+                            </div>
+                            <div>
+                                <input type="text" class="form-control" style="width: 218px;height: 34px;"
+                                       id="cycle_section" name="cycle_section" value=""
+                                       placeholder="节点区间" readonly>
+                            </div>
+
+                        </td>
+                    </tr>
+                    <tr class="form-group">
+                        <th>节点描述</th>
+                        <td colspan="5">
+                            <div class="form-group"><textarea class="form-control" id="cycle_describe"
+                                                              name="cycle_describe" placeholder="输入节点描述信息"></textarea>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="form-group">
+                        <th>上传图片</th>
+                        <td colspan="5">
+                            <div class="form-group"><input type="file" class="form-control" id="cycle_img"
+                                                           name="cycle_img"
+                                                           placeholder="节点图片描述"></div>
+                        </td>
+                    </tr>
+
+                    <tr class="form-group">
+                        <th>图片预览</th>
+                        <td colspan="5">
+                            <div class="form-group">
+                                <img src="" class="img_browser" id="img_browser" alt="图片预览">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="form-group">
+                        <th>是否生效</th>
+                        <td>
+                            <label>
+                                <input type="radio" name="cycle_status" class="flat-red" checked value="0"> 不生效
+                            </label>
+                            <label>
+                                <input type="radio" name="cycle_status" class="flat-red" value="1"> 生效
+                            </label>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+        <div class="box-footer clearfix text-center">
+            <button type="submit" class="btn btn-primary" id="submit"><i class="fa fa-save"></i> 提交</button>
+            <a href="#" onClick="javascript:history.back(-1);" class="btn btn-default"><i class="fa fa-arrow-left"></i>
+                返回</a>
+        </div>
+    </div>
+    </body>
+@endsection
+
+@section("script")
+    <!-- DataTables -->
+    <script src="{{asset("/resources/plugins/datatables/jquery.dataTables.min.js")}}"></script>
+    <script src="{{asset("/resources/plugins/datatables/dataTables.bootstrap.min.js")}}"></script>
+    <script src="{{asset("/resources/plugins/jQuery/jquery.validate.min.js")}}"></script>
+    <script src="{{asset("/resources/plugins/jQuery/additional-methods.min.js")}}"></script>
+    <script src="{{asset("/resources/js/crop/cropCycle.js")}}"></script>
+    <script src="{{asset("/resources/plugins/uploadify/jquery.uploadify.min.js")}}"></script>
+    <script src="{{asset('/resources/plugins/daterangepicker/moment.js')}}"></script>
+    <script src="{{asset("/resources/plugins/daterangepicker/daterangepicker-dob.js")}}"></script>
+@endsection
+
+@section("js")
+    <script type="text/javascript">
+        //待上传图片预览
+        $("#cycle_img").change(function () {
+            var objUrl = getObjectURL(this.files[0]);
+            if (objUrl) {
+                $("#img_browser").attr("src", objUrl);
+            }
+        });
+
+        //建立一個可存取到該file的url
+        function getObjectURL(file) {
+            var url = null;
+            window.createObjectURL = undefined;
+            if (window.createObjectURL != undefined) { // basic
+                url = window.createObjectURL(file);
+            } else if (window.URL != undefined) { // mozilla(firefox)
+                url = window.URL.createObjectURL(file);
+            } else if (window.webkitURL != undefined) { // webkit or chrome
+                url = window.webkitURL.createObjectURL(file);
+            }
+            return url;
+        }
+    </script>
+@endsection
