09c90f64b5c72cf63aeed5b50f5017484eedc5be
warthur <warthur@163.com>
2017-05-08 09:44:24 +0800

    wechat scan web

---
+++ resources/views/public/wechatWeb.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head> 
+	<meta charset="utf-8"> 
+	<meta content="yes" name="apple-mobile-web-app-capable"> 
+	<meta content="yes" name="apple-touch-fullscreen"> 
+	<meta content="telephone=no,email=no" name="format-detection">
+	
+
+	<script src="js/flexible_css.debug.js"></script> 
+	<script src="js/flexible.debug.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/index.css">	
+</head> 
+
+<script>
+
+</script>
+<body>
+
+<header id="header" class="clearfix">太仓市万丰村农产品质量查询</header>
+<div id="content">
+	<div class="product_select">
+		<div class="product_select_title tc">
+			<span>太仓市万丰村农产品质量查询</span>
+		</div>	
+		<p class="tc">黄豆</p>
+		<p class="tc">这个是黄豆吗？只是需要测试看看</p>
+
+	</div>
+	<div class="split tc clearfix">
+		<div class="l seperate"></div>
+		<div class="cube"></div>
+		<div class="r seperate"></div>
+	</div>
+	
+	<div class="product_cycle tc">
+		农产品生长周期
+	</div>
+	<div class="product_point tc">
+		↓↓↓
+	</div>
+	<div class="repeat_wrap">
+		<!-- 重复模块 -->
+		<div class="product_classify">
+			<div class="bd_left"></div>
+			<div class="bd_title clearfix">
+				<div class="bd_circle tc l">*</div>
+				<div class="bd_title_letter l">种子产品</div>
+			</div>
+			<div class="bd_content clearfix">
+				<div class="bd_empty tc l"></div>
+				<div class="bd_content_letter l">黄豆</div>
+			</div>
+		</div>
+		<!-- 重复模块 -->
+		<!-- 重复模块 -->
+		<div class="product_classify">
+			<div class="bd_left"></div>
+			<div class="bd_title clearfix">
+				<div class="bd_circle tc l">*</div>
+				<div class="bd_title_letter l">种子产品</div>
+			</div>
+			<div class="bd_content clearfix">
+				<div class="bd_empty tc l"></div>
+				<div class="bd_content_letter l">黄豆</div>
+			</div>
+		</div>
+		<!-- 重复模块 -->
+	</div>
+	<div class="split tc clearfix">
+		<div class="l seperate"></div>
+		<div class="cube"></div>
+		<div class="r seperate"></div>
+	</div>
+
+	<div class="border_contain">
+	    <div class="border_outer">
+	    	<div class="border_inner">
+	    		<div class="tc border_title">
+	    			生长阶段的施药及施肥情况
+	    		</div>
+	    	</div>
+	    </div>
+	</div>
+</div>
+<footer id="footer">
+	<div class="clearfix">
+		<div class="l">
+			<h3 class="footer_title">周期描述</h3>
+			<p>1、测试发现提交没有用</p>
+		</div>
+		<div class="r">
+			
+		</div>
+	</div>
+	<div class="connectUs tc">
+		联系我们
+	</div>
+</footer>
+		
+</body>
+</html>
